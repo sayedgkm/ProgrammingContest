2024d9baf3d64d30406ddacf142e08a5600bd29d
Sayed Ahmad <sayedgkm@gmail.com>
2018-02-23 17:21:55 +0600

    Vjudge

---
+++ Vjudge/BAPS Monthly Team Practice Contest 1/Problem E. Cryptography.cpp
@@ -0,0 +1,78 @@
+/// Bismillahir-Rahmanir-Rahim
+#include <bits/stdc++.h>
+#define        ll                              long long int
+#define        FOR(x,y,z)                      for(int x=y;x<z;x++)
+#define        pii                             pair<int,int>
+#define        pll                             pair<ll,ll>
+#define        CLR(a)                          memset(a,0,sizeof(a))
+#define        SET(a)                          memset(a,-1,sizeof(a))
+#define        N                               200010
+#define        M                               1000000007
+#define        pi                              acos(-1.0)
+#define        ff                              first
+#define        ss                              second
+#define        pb                              push_back
+#define        inf                              (1e9)+1000
+#define        eps                             1e-9
+#define        ALL(x)                          x.begin(),x.end()
+using namespace std;
+int dx[]={0,0,1,-1,-1,-1,1,1};
+int dy[]={1,-1,0,0,-1,1,1,-1};
+template < class T> inline T biton(T n,T pos){return n |((T)1<<pos);}
+template < class T> inline T bitoff(T n,T pos){return n & ~((T)1<<pos);}
+template < class T> inline T ison(T n,T pos){return (bool)(n & ((T)1<<pos));}
+template < class T> inline T gcd(T a, T b){while(b){a%=b;swap(a,b);}return a;}
+template <typename T> string NumberToString ( T Number ) { ostringstream ss; ss << Number; return ss.str(); }
+inline int nxt(){int aaa;scanf("%d",&aaa);return aaa;}
+inline ll lxt(){ll aaa;scanf("%lld",&aaa);return aaa;}
+inline double dxt(){double aaa;scanf("%lf",&aaa);return aaa;}
+template <class T> inline T bigmod(T p,T e,T m){T ret = 1;
+for(; e > 0; e >>= 1){
+    if(e & 1) ret = (ret * p) % m;p = (p * p) % m;
+} return (T)ret;}
+#ifdef sayed
+     #define debug(...) __f(#__VA_ARGS__, __VA_ARGS__)
+    template < typename Arg1 >
+    void __f(const char* name, Arg1&& arg1){
+        cerr << name << " is " << arg1 << std::endl;
+    }
+    template < typename Arg1, typename... Args>
+    void __f(const char* names, Arg1&& arg1, Args&&... args){
+        const char* comma = strchr(names+1, ',');
+        cerr.write(names, comma - names) << " is " << arg1 <<" | ";
+        __f(comma+1, args...);
+    }
+#else
+    #define debug(...)
+#endif
+///******************************************START******************************************
+int ar[N];
+int main(){
+    #ifdef sayed
+
+    #endif
+    //ios_base::sync_with_stdio(false);
+    //cin.tie(0);
+    freopen("crypto.in","r",stdin);
+    freopen("crypto.out","w",stdout);
+    int n= nxt();
+    int a = nxt();
+    int b= nxt();
+    int ans = 0;
+    int xx ,yy;
+    for(int mask =0;mask<=n;mask++) {
+        for(int i = mask; i > 0; i = (i-1) & mask){
+        	int x = mask;
+        	int y= i;
+        	int tmp = ((a*x)+(b*y))^((a*y)+(b*x));
+        	if(tmp>=ans) {
+                ans = tmp;
+                xx = x;
+                yy = y;
+        	}
+     }
+    }
+    printf("%d %d\n",xx,yy);
+
+    return 0;
+}
