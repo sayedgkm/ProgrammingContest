a380c707f6005e197f3184a75b58fa80acac5310
sayedgkm <sayedgkm@gmail.com>
2019-07-20 17:04:11 +0600

    - Codeforces div2 573, div2 574, Educational 68

---
+++ Codeforces/Codeforces Round #574 (Div. 2)/D2.cpp
@@ -0,0 +1,114 @@
+///Bismillahir-Rahmanir-Rahim
+#include <bits/stdc++.h>
+#define        ll                              long long int
+#define        FOR(x,y,z)                      for(int x=y;x<z;x++)
+#define        pii                             pair<int,int>
+#define        pll                             pair<ll,ll>
+#define        CLR(a)                          memset(a,0,sizeof(a))
+#define        SET(a)                          memset(a,-1,sizeof(a))
+#define        N                               1000010
+#define        M                               998244353
+#define        pi                              acos(-1.0)
+#define        ff                              first
+#define        ss                              second
+#define        pb                              push_back
+#define        inf                              (1e9)+1000
+#define        eps                             1e-9
+#define        ALL(x)                          x.begin(),x.end()
+using namespace std;
+int dx[]={0,0,1,-1,-1,-1,1,1};
+int dy[]={1,-1,0,0,-1,1,1,-1};
+template < class T> inline T biton(T n,T pos){return n |((T)1<<pos);}
+template < class T> inline T bitoff(T n,T pos){return n & ~((T)1<<pos);}
+template < class T> inline T ison(T n,T pos){return (bool)(n & ((T)1<<pos));}
+template < class T> inline T gcd(T a, T b){while(b){a%=b;swap(a,b);}return a;}
+template <typename T> string NumberToString ( T Number ) { ostringstream ss; ss << Number; return ss.str(); }
+inline int nxt(){int aaa;scanf("%d",&aaa);return aaa;}
+inline ll lxt(){ll aaa;scanf("%lld",&aaa);return aaa;}
+inline double dxt(){double aaa;scanf("%lf",&aaa);return aaa;}
+template <class T> inline T bigmod(T p,T e,T m){T ret = 1;
+for(; e > 0; e >>= 1){
+    if(e & 1) ret = (ret * p) % m;p = (p * p) % m;
+} return (T)ret;}
+#ifdef sayed
+     #define debug(...) __f(#__VA_ARGS__, __VA_ARGS__)
+    template < typename Arg1 >
+    void __f(const char* name, Arg1&& arg1){
+        cerr << name << " is " << arg1 << std::endl;
+    }
+    template < typename Arg1, typename... Args>
+    void __f(const char* names, Arg1&& arg1, Args&&... args){
+        const char* comma = strchr(names+1, ',');
+        cerr.write(names, comma - names) << " is " << arg1 <<" | ";
+        __f(comma+1, args...);
+    }
+#else
+    #define debug(...)
+#endif
+///******************************************START******************************************
+string s[N];
+ll Pow[50];
+ll len[N];
+ll get(ll x,int pos) {
+
+    return (x*Pow[pos-1])%M;
+}
+int main(){
+    #ifdef sayed
+    //freopen("out.txt","w",stdout);
+    // freopen("in.txt","r",stdin);
+    #endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    Pow[0] = 1LL;
+    for(int i = 1;i<=30;i++) {
+        Pow[i] = Pow[i-1]*10LL;
+        Pow[i]%=M;
+    }
+    int n;
+    cin>>n;
+    for(int i = 0;i<n;i++) {
+        cin>>s[i];
+        len[(int)s[i].size()] ++;
+    }
+    ll ans = 0;
+    for(int i = 0;i<n;i++) {
+        int pos = 0;
+        //debug(s[i]);
+        for(int k = s[i].size()-1;k>=0;k--){
+            ll val = s[i][k]-'0';
+            pos++;
+            ll subans = 0;
+            for(int j = 1;j<=10;j++) {
+                ll tot = len[j];
+                if(j>=pos) {
+                    subans = get(val,pos*2);
+                  //  if(tot) debug(j,pos*2);
+                } else {
+                    subans = get(val, j*2 + (pos-j));
+                   // if(tot) debug(j,j*2+(pos-j));
+                }
+                subans = subans * tot;
+                subans%=M;
+                ans+=subans;
+                ans%=M;
+
+                /// Y
+
+                if(j>=pos) {
+                    subans = get(val,(pos*2)-1);
+                } else {
+                    subans = get(val, (j*2) + (pos-j));
+                }
+                subans = subans * tot;
+                subans%=M;
+                ans+=subans;
+                ans%=M;
+            }
+
+        }
+    }
+    cout<<ans<<endl;
+    return 0;
+}
+
